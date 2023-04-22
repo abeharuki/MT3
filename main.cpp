73b0523f0340f23b3b1a4143f6d7b1b093e6e060
haruki <haruki@MSI>
2023-04-23 01:14:37 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -15,24 +15,83 @@ struct Matrix4x4 {
 };
 
 Matrix4x4 MakeTranselateMatrix(const Vector3& translate) {
-
+	Matrix4x4 MakeTranslateMatrix;
+	MakeTranslateMatrix.m[0][0] = 1;
+	MakeTranslateMatrix.m[0][1] = 0;
+	MakeTranslateMatrix.m[0][2] = 0;
+	MakeTranslateMatrix.m[0][3] = 0;
+	MakeTranslateMatrix.m[1][0] = 0;
+	MakeTranslateMatrix.m[1][1] = 1;
+	MakeTranslateMatrix.m[1][2] = 0;
+	MakeTranslateMatrix.m[1][3] = 0;
+	MakeTranslateMatrix.m[2][0] = 0;
+	MakeTranslateMatrix.m[2][1] = 0;
+	MakeTranslateMatrix.m[2][2] = 1;
+	MakeTranslateMatrix.m[2][3] = 0;
+	MakeTranslateMatrix.m[3][0] = translate.x;
+	MakeTranslateMatrix.m[3][1] = translate.y;
+	MakeTranslateMatrix.m[3][2] = translate.z;
+	MakeTranslateMatrix.m[3][3] = 1;
+	return MakeTranslateMatrix;
 };
 
 Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
-
+	Matrix4x4 MakeScaleMatrix;
+	MakeScaleMatrix.m[0][0] = scale.x;
+	MakeScaleMatrix.m[0][1] = 0;
+	MakeScaleMatrix.m[0][2] = 0;
+	MakeScaleMatrix.m[0][3] = 0;
+	MakeScaleMatrix.m[1][0] = 0;
+	MakeScaleMatrix.m[1][1] = scale.y;
+	MakeScaleMatrix.m[1][2] = 0;
+	MakeScaleMatrix.m[1][3] = 0;
+	MakeScaleMatrix.m[2][0] = 0;
+	MakeScaleMatrix.m[2][1] = 0;
+	MakeScaleMatrix.m[2][2] = scale.z;
+	MakeScaleMatrix.m[2][3] = 0;
+	MakeScaleMatrix.m[3][0] = 0;
+	MakeScaleMatrix.m[3][1] = 0;
+	MakeScaleMatrix.m[3][2] = 0;
+	MakeScaleMatrix.m[3][3] = 1;
+	return MakeScaleMatrix;
 };
 
 Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
-
+	Vector3 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w!= 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
 };
 
 
+
+static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
+
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
 	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
 	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
 	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
 	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+void MatrixScreenPrintf(int x, int y, const  Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s\n", label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight + 20, "%6.02f", matrix.m[row][column]
+			);
+
+		}
+	}
 }
 
 
@@ -81,9 +140,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		
-
-
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, 20, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5+20, scaleMatrix, "scaleMatrix");
 
 		///
 		/// ↑描画処理ここまで
