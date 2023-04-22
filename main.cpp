a9fd94f760c0621a5c464dd73b16670227b3f620
haruki <haruki@MSI>
2023-04-23 00:25:45 +0900

    screenprint

--- main.cpp
+++ main.cpp
@@ -27,6 +27,15 @@ Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 };
 
 
+static const int kColumnWidth = 60;
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
