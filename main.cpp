5e8d9c90fdfdd8a620390aa5bd01a88465a00296
haruki <haruki@MSI>
2023-04-28 11:02:32 +0900

    関数の作成

--- main.cpp
+++ main.cpp
@@ -14,6 +14,74 @@ struct Matrix4x4 {
 	float m[4][4];
 };
 
+//投資投影行列
+Matrix4x4 MakePerspectiverFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4  MakePerspectiverFovMatrix;
+	MakePerspectiverFovMatrix.m[0][0] = 1 / aspectRatio * 1 / tan(fovY / 2);
+	MakePerspectiverFovMatrix.m[0][1] = 0.0f;
+	MakePerspectiverFovMatrix.m[0][2] = 0.0f;
+	MakePerspectiverFovMatrix.m[0][3] = 0.0f;
+
+	MakePerspectiverFovMatrix.m[1][0] = 0.0f;
+	MakePerspectiverFovMatrix.m[1][1] = 1 / tan(fovY / 2);
+	MakePerspectiverFovMatrix.m[1][2] = 0.0f;
+	MakePerspectiverFovMatrix.m[1][3] = 0.0f;
+
+	MakePerspectiverFovMatrix.m[2][0] = 0.0f;
+	MakePerspectiverFovMatrix.m[2][1] = 0.0f;
+	MakePerspectiverFovMatrix.m[2][2] = nearClip / nearClip - farClip;
+	MakePerspectiverFovMatrix.m[2][3] = 1.0f;
+
+	MakePerspectiverFovMatrix.m[3][0] = 0.0f;
+	MakePerspectiverFovMatrix.m[3][1] = 0.0f;
+	MakePerspectiverFovMatrix.m[3][2] = -farClip * nearClip / (nearClip - farClip);
+	MakePerspectiverFovMatrix.m[3][3] = 0.0f;
+
+}
+
+//正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom,float nerClip,float farClip) {
+
+	assert(left != right);
+	assert(top != bottom);
+	Matrix4x4 MakeOrthographicMatrix;
+	MakeOrthographicMatrix.m[0][0] = 2.0f / (right - left);
+	MakeOrthographicMatrix.m[0][1] = 0.0f;
+	MakeOrthographicMatrix.m[0][2] = 0.0f;
+	MakeOrthographicMatrix.m[1][0] = 0.0f;
+	MakeOrthographicMatrix.m[1][1] = 2.0f / (top - bottom);
+	MakeOrthographicMatrix.m[1][2] = 0.0f;
+	MakeOrthographicMatrix.m[2][0] = (left + right) / (left - right);
+	MakeOrthographicMatrix.m[2][1] = (top + bottom) / (bottom - top);
+	MakeOrthographicMatrix.m[2][2] = 1.0f;
+	return MakeOrthographicMatrix;
+
+}
+
+//ビューポート行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height,float minDepth,float maxDepth) {
+	Matrix4x4 MakeViewportMatrix;
+	MakeViewportMatrix.m[0][0] = width / 2.0f;
+	MakeViewportMatrix.m[0][1] = 0.0f;
+	MakeViewportMatrix.m[0][2] = 0.0f;
+	MakeViewportMatrix.m[0][3] = 0.0f;
+	MakeViewportMatrix.m[1][0] = 0.0f;
+	MakeViewportMatrix.m[1][1] = -height / 2.0f;
+	MakeViewportMatrix.m[1][2] = 0.0f;
+	MakeViewportMatrix.m[1][3] = 0.0f;
+	MakeViewportMatrix.m[2][0] = 0.0f;
+	MakeViewportMatrix.m[2][1] = 0.0f;
+	MakeViewportMatrix.m[2][2] = maxDepth - minDepth;
+	MakeViewportMatrix.m[2][3] = 0.0f;
+	MakeViewportMatrix.m[3][0] = left + (width / 2.0f);
+	MakeViewportMatrix.m[3][1] = top + (height / 2.0f);
+	MakeViewportMatrix.m[3][2] = minDepth;
+	MakeViewportMatrix.m[3][3] = 1.0f;
+	return MakeViewportMatrix;
+}
+
+
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
